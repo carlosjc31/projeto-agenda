9549acdd662fa7174d177890fdf5cde6f4837bfe
carlosjc31 <carlosjc31@hotmail.com>
2024-11-15 09:26:29 -0300

    9º commit, criando a rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FormularioAgendaComponent } from './formulario-agenda/formulario-agenda.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TabelaAgendaComponent } from './tabela-agenda/tabela-agenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/agendas', pathMatch: 'full' },
+  { path: 'agendas', Component: TabelaAgendaComponent },
+  { path: 'agenda/:id', Component: FormularioAgendaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
