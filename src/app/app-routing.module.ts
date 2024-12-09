93981ae15d853f2c52233d55ab2ecdff4d8c621d
carlosjc31 <carlosjc31@hotmail.com>
2024-12-09 17:43:59 -0300

    9º criando as rotas no app.routing e o navbar no app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaAgendaComponent } from './lista-agenda/lista-agenda.component';
+import { FormAgendaComponent } from './form-agenda/form-agenda.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'agenda', pathMatch: 'full' },
   { path: 'agendas', component: ListaAgendaComponent },
-  { path: 'agenda/:id', component: ListaAgendaComponent },
-
+  { path: 'agenda/:id', component: FormAgendaComponent },
+  { path: 'agenda', component: FormAgendaComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
